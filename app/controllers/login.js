ba8f2316189e2192a70e6b72e19f0741d2284f26
MennovanLochem <menno696@gmail.com>
2018-12-11 11:43:16 +0100

    Created error message with false credentials

---
+++ app/controllers/login.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+import { inject } from '@ember/service';
+
+export default Controller.extend({
+  session: inject(),
+
+  actions: {
+    login(email, password) {
+      this.get('session').authenticate('authenticator:application', email, password)
+        .catch((reason) => {
+          this.set('errorMessage', reason.errors[0].detail);
+        });
+    }
+  }
+});
--- app/routes/login.js
+++ app/routes/login.js
@@ -2,11 +2,5 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
 export default Route.extend({
-  session: inject(),
 
-  actions: {
-    login(email, password) {
-      this.get('session').authenticate('authenticator:application', email, password);
-    }
-  }
 });
