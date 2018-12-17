5cfa7332737c65d86cad78f7008f2ada712a1a6a
Boris Bolder <borisb_2@hotmail.com>
2018-12-17 13:42:34 +0100

    Fixed Ember.set error

--- app/routes/classrooms/new.js
+++ app/routes/classrooms/new.js
@@ -1,13 +1,12 @@
 import Route from '@ember/routing/route';
-import * as Ember from "../../../dist/assets/vendor";
-
+import { set } from '@ember/object';
 export default Route.extend({
   model() {
     return this.store.createRecord('classroom');
   },
   actions: {
     saveModel() {
-      Ember.set(this.currentModel, 'reservedSpots', 0);
+      set(this.currentModel, 'reservedSpots', 0);
       this.currentModel.save();
       this.transitionTo('classrooms');
     },
