f1e0134c331c0a47399c343c8f758ba795c9eb8b
rdrb12 <4ppels4p@live.nl>
2018-12-17 19:52:06 +0100

    comments for the code

--- app/routes/studentgroups/new.js
+++ app/routes/studentgroups/new.js
@@ -1,10 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  //return create record model of studentgroup.
   model () {
     return this.store.createRecord('studentgroup');
   },
   actions: {
+    //Save the current model and transition to /studentgroups
     saveModel() {
       this.currentModel.save();
       this.transitionTo('studentgroups');
