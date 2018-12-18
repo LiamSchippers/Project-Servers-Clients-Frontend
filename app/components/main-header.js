bb92da886f44ec8b95d544de90d1a676cb44e034
Tom Oude Rengerink <tomouderengerink@gmail.com>
2018-12-18 19:42:13 +0100

    Re-added logout

--- app/components/main-header.js
+++ app/components/main-header.js
@@ -1,9 +1,14 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+  session: inject('session'),
   actions: {
     toggleUserDropdown() {
       this.toggleProperty('dropdownOpen');
+    },
+    logOut() {
+      this.get('session').invalidate();
     }
   }
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,5 @@
 import Controller from '@ember/controller';
-import { inject } from '@ember/service';
 
 export default Controller.extend({
-  session: inject('session'),
 
-  actions: {
-    logOut() {
-      this.get('session').invalidate();
-    }
-  }
 });
