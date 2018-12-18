093cee53dd7b136ddd584048241e23df9fe47ae6
Lloyd <lloydwaldt@hotmail.com>
2018-12-18 12:06:19 +0100

    opzet

--- app/router.js
+++ app/router.js
@@ -18,6 +18,7 @@ Router.map(function() {
   this.route('classroom', function() {});
   this.route('login');
   this.route('studentgroup', function() {});
+  this.route('new-reservation', function() {});
 });
 
 export default Router;
