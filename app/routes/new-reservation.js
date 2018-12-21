92f4eb5492fd8aff3ccc3d7d643a9ee6e2b6bfe9
broodjekroket <voernaamin@gmail.com>
2018-12-21 14:12:26 +0100

    added saveReservation action

--- app/routes/new-reservation.js
+++ app/routes/new-reservation.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model () {
+    return this.store.createRecord('reservation');
+  },
+  actions: {
+    saveReservation() {
+      this.currentModel.save();
+      this.transitionTo('index');
+    }
+  }
 });
