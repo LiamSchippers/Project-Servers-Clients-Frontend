23097fc578eb970a077c66c940bef42ce45e4b28
Lloyd <lloydwaldt@hotmail.com>
2019-01-02 20:23:52 +0100

    Alle atributen toegevoegd waarbij als het goed is de value's worden opgeslagen in het Reservation Model. Het submitten / opslaan werkt nog niet.

--- app/routes/reservations/new.js
+++ app/routes/reservations/new.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model () {
+    return this.store.createRecord('reservation');
+  },
+  actions: {
+    saveReservation() {
+      //test
+      this.currentModel.save();
+      this.transitionTo('reservations');
+    }
+  }
 });
