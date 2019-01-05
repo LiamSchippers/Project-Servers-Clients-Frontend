19dfce9c71ce3fd9e82798c106262b31b86bfeca
Frank Nijhof <frank_nijhof_20@hotmail.com>
2019-01-05 15:59:30 +0100

    Revert "frontend:"
    This reverts commit 9daf1559695b197bdf48909b646475650af34220.

--- app/routes/studentgroups/new.js
+++ app/routes/studentgroups/new.js
@@ -1,23 +1,15 @@
 import Route from '@ember/routing/route';
-import studentgroup from "../../models/studentgroup";
 
 export default Route.extend({
-
   //return create record model of studentgroup.
-  model() {
+  model () {
     return this.store.createRecord('studentgroup');
   },
-
   actions: {
+    //Save the current model and transition to /studentgroups
     saveModel() {
-      this.currentModel.save().then(() => {
-        this.transitionTo('studentgroups.show', this.currentModel.id);
-      })
-    },
-    cancelModel() {
-      this.currentModel.destroyRecord().then(() => {
-        this.transitionTo('studentgroups.index')
-      })
+      this.currentModel.save();
+      this.transitionTo('studentgroups');
     }
   }
 });
