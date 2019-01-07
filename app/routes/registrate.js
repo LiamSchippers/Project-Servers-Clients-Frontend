eea718de95a8730b4c2cb44aabdff54de9691906
Thomas Tunc <thomastunc@hotmail.com>
2019-01-07 15:35:37 +0100

    Added post feature

--- app/routes/registrate.js
+++ app/routes/registrate.js
@@ -1,18 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model () {
+  model() {
     return this.store.createRecord('ExtendedUser');
   },
   actions: {
     signUpRoute(passwordCheck) {
-      if (passwordCheck === this.currentModel.password) {
-        this.currentModel.save();
-        this.transitionTo('index');
-      } else {
-        console.log("Vieze tyfuslijer");
-        this.transitionTo('index');
-      }
+      this.currentModel.save();
+      this.transitionTo('index');
     }
   }
 });
