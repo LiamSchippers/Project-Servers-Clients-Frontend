67138e9d99c22bf62f5a6acabdea54fe3480399e
ruben <rubenoonk@gmail.com>
2019-01-08 12:19:22 +0100

    Send classrooms and reservations to the classroom index page

--- app/routes/classrooms/index.js
+++ app/routes/classrooms/index.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('Reservation');
+      return Ember.RSVP.hash({
+          classrooms: this.store.findAll('classroom'),
+          reservations: this.store.findAll('reservation')
+    });
   }
 });
