4691d4f2e26e0b02058347f25a6a42a53a0c7657
rdrb12 <4ppels4p@live.nl>
2019-01-08 14:56:15 +0100

    frontend;
    add student to studentgroup via membership.

--- app/controllers/studentgroups/edit.js
+++ app/controllers/studentgroups/edit.js
@@ -1,26 +1,54 @@
 import Controller from '@ember/controller';
+import EmberObject from '@ember/object';
+
 
 export default Controller.extend({
   actions: {
-    filterByStudent(param) {
-      if (param !== '') {
-        return this.store.query('extended-user', {
-          filter: {
-            where: {
-              username: {ilike: '%'+ param + '%'},
-              realm: 'student'
+
+      filterByStudent(param) {
+        if (param !== '') {
+          return this.store.query('extended-user', {
+            filter: {
+              where: {
+                username: {ilike: '%' + param + '%'},
+                realm: 'student'
+              }
             }
-          }
-        })
-      } else{
-        return this.store.query('extended-user', {
-          filter: {
-            where: {
-              realm: 'student'
+          })
+        } else {
+          return this.store.query('extended-user', {
+            filter: {
+              where: {
+                realm: 'student'
+              }
             }
-          }
-        })
+          })
+        }
+      },
+      addStudent(student) {
+        var model = this.get('model');
+
+        var membership = this.store.createRecord('membership',{
+          studentgroupId: model.groupID,
+          userId: student.userId,
+          user: student,
+          studentgroup: model
+        });
+        membership.save();
+
+
+        //
+        // console.log(student);
+
+        // model.memberships.pushObject(membership);
+        // this.store.commit();
+        // console.log(model);
+
+      },
+      goBack() {
+        var model = this.get('model');
+        this.transitionToRoute('studentgroups.show', model.id);
       }
-    }
+
   }
 });
