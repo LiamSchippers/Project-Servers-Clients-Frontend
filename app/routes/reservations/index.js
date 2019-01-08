0df623ae820e94d2b6733a74b56a5a1bcaa24f10
jaspersauerwald <jaspersauerwald@gmail.com>
2019-01-08 12:31:37 +0100

    Reservations can be deleted
    -button added for deleting reservation
    -reservations/index.js handles deleting the reservation

--- app/routes/reservations/index.js
+++ app/routes/reservations/index.js
@@ -3,5 +3,13 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.findAll('reservation');
+  },
+
+  actions: {
+    deleteReservation(reservation){
+      reservation.deleteRecord();
+      reservation.get('isDeleted');
+      reservation.save();
+    }
   }
 });
