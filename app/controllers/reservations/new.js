1ee2a5b9df79328810f6278a7f15a7b8618d7b02
Tom Oude Rengerink <tomouderengerink@gmail.com>
2019-01-14 21:08:10 +0100

    Start & End Time

--- app/controllers/reservations/new.js
+++ app/controllers/reservations/new.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import { set } from '@ember/object';
 
 export default Controller.extend({
   studentgroupId: null,
@@ -13,17 +12,18 @@ export default Controller.extend({
   classrooms: computed( function() {
     return this.get('store').findAll('classroom');
   }),
-
-  init() {
-    this._super(...arguments);
-    //$('#date').datepicker();
-  },
   actions: {
     selectStudentGroup: function(selected) {
       this.set('studentgroupId', selected);
     },
     selectClassroom: function(selected) {
       this.set('classroomId', selected);
+    },
+    selectStartHour: function(selected) {
+      this.set('startHour', selected);
+    },
+    selectEndHour: function(selected) {
+      this.set('endHour', selected);
     },
     saveModel() {
       const model = this.get('model');
@@ -32,8 +32,6 @@ export default Controller.extend({
       const classroomId = this.get('classroomId');
       store.findRecord('studentgroup', studentGroupId).then(function(studentGroup) {
         store.findRecord('classroom', classroomId).then(function(classroom) {
-          console.log(studentGroup.groupName);
-          console.log(classroom.roomNumber);
           model.set('studentgroup', studentGroup);
           model.set('classroom', classroom);
           model.save();
