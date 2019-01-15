87a266baad242baf617f6a2d8b7eacf640b90358
broodjekroket <voernaamin@gmail.com>
2019-01-15 12:42:36 +0100

    merge conflicts resolved

--- app/router.js
+++ app/router.js
@@ -22,7 +22,6 @@ Router.map(function() {
   this.route('classroom', function() {});
   this.route('login');
   this.route('studentgroup', function() {});
-  this.route('new-reservation', function() {});
 });
 
 export default Router;
