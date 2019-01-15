5ecea1de578e3649fb4167b2420bc15aee9f6d66
Anferney1205 <454710@student.saxion.nl>
2019-01-15 12:18:19 +0100

    Authorization mixins

---
+++ app/mixins/authorization-admin-route-mixin.js
@@ -0,0 +1,20 @@
+import {inject} from '@ember/service';
+import Mixin from '@ember/object/mixin';
+
+export default Mixin.create({
+  session: inject(),
+  /**
+   * In some cases, access checking in #beforeModel is not sufficient.
+   * Overriding #beforeModel is not a feasible options, because this also overrides other beforeModel hooks higher up
+   * in the dependency tree. Using this flag, the access check in the #beforeModel hook can be disabled.
+   */
+  skipBeforeModelAccessCheck: false,
+  beforeModel(transition){
+    let session = this.session;
+    if(session.get('data.role') === "admin") {
+      return transition;
+    }else{
+      this.transitionTo('index');
+    }
+  }
+});
