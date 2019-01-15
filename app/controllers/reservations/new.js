7a832a26bfa5c7ab84347359c9bf2481e8a8e2f9
broodjekroket <voernaamin@gmail.com>
2019-01-15 12:33:47 +0100

    added return value

--- app/controllers/reservations/new.js
+++ app/controllers/reservations/new.js
@@ -10,26 +10,31 @@ export default Controller.extend({
   classrooms: computed(function () {
     return this.get('store').findAll('classroom');
   }),
-  availableClassrooms: computed('classrooms', 'studentgroups', function () {
-    let amountOfGroupMembers;
-    let availableClassRooms = null;
-    this.studentgroups.then((studentgroups)=>{
-      studentgroups.forEach((studentgroup) => {
-        if (studentgroup.get('groupName') === 'groupName'){
-          amountOfGroupMembers = studentgroup.get('amountOfGroupMembers');
-        }
-      });
-    this.classrooms.then((classrooms)=>{
-      console.log(classrooms);
-      classrooms.forEach((classroom)=>{
-        let availableSpots = classroom.get('availableSpots');
-        if (availableSpots >= amountOfGroupMembers){
-          console.log("deze classroom kan: " + classroom);
-          availableClassRooms.push(classroom);
-        }
-      });
-      console.log("availableclassrooms: " + availableClassRooms);
-    });
-    });
-  })
+  // availableClassrooms: computed('classrooms', 'studentgroups', function () {
+  //   let _this = this;
+  //   return new Promise(function(resolve, reject) {
+  //     let amountOfGroupMembers;
+  //     let availableClassRooms = [];
+  //     _this.studentgroups.then((studentgroups) => {
+  //       studentgroups.forEach((studentgroup) => {
+  //         if (studentgroup.get('groupName') === 'groupName') {
+  //           amountOfGroupMembers = studentgroup.get('amountOfGroupMembers');
+  //         }
+  //       });
+  //       _this.classrooms.then((classrooms) => {
+  //         console.log(classrooms);
+  //         classrooms.forEach((classroom) => {
+  //           let availableSpots = classroom.get('availableSpots');
+  //           if (availableSpots >= amountOfGroupMembers) {
+  //             console.log("deze classroom kan: " + classroom);
+  //             availableClassRooms.push(classroom);
+  //           }
+  //         });
+  //       }).finally(() => {
+  //         resolve(availableClassRooms);
+  //       });
+  //     });
+  //     console.log(availableClassRooms);
+  //   });
+  // })
 });
