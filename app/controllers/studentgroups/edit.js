38b54dd3d41a3239a2e71f54ac03ab9d8e7ed637
luuklelifeld <luukleli2@hotmail.com>
2019-01-18 15:51:23 +0100

    Added deletion for memberships

--- app/controllers/studentgroups/edit.js
+++ app/controllers/studentgroups/edit.js
@@ -7,64 +7,69 @@ import MutableArray from '@ember/array/mutable';
 export default Controller.extend({
   actions: {
 
-      filterByStudent(param) {
-        var model = this.get('model');
-        if (param !== '') {
-          return this.store.query('extended-user', {
-            filter: {
-              where: {
-                username: {ilike: '%' + param + '%'},
-                role: 'student'
-              }
+    filterByStudent(param) {
+      var model = this.get('model');
+      if (param !== '') {
+        return this.store.query('extended-user', {
+          filter: {
+            where: {
+              username: {ilike: '%' + param + '%'},
+              role: 'student'
             }
-          }).then(function(students) {
-            // filter students based on already present ones.
-            var freeStudents = students.toArray();
+          }
+        }).then(function (students) {
+          // filter students based on already present ones.
+          var freeStudents = students.toArray();
 
-            students.forEach(function(student){
-              if (model.memberships.findBy('user._belongsToState.canonicalState.id', student.get('id'))){
-                freeStudents.removeObject(student);
-              }
-            });
-            return freeStudents;
-          });
-        } else {
-          return this.store.query('extended-user', {
-            filter: {
-              where: {
-                role: 'student'
-              }
+          students.forEach(function (student) {
+            if (model.memberships.findBy('user._belongsToState.canonicalState.id', student.get('id'))) {
+              freeStudents.removeObject(student);
             }
-          }).then(function(students){
-            // filter students based on already present ones.
-            var freeStudents = students.toArray();
-
-            students.forEach(function(student){
-              if (model.memberships.findBy('user._belongsToState.canonicalState.id', student.get('id'))){
-                freeStudents.removeObject(student);
-              }
-            });
-            return freeStudents;
           });
-        }
-      },
-      addStudent(student) {
-        var model = this.get('model');
+          return freeStudents;
+        });
+      } else {
+        return this.store.query('extended-user', {
+          filter: {
+            where: {
+              role: 'student'
+            }
+          }
+        }).then(function (students) {
+          // filter students based on already present ones.
+          var freeStudents = students.toArray();
 
-        var membership = this.store.createRecord('membership',{
-          studentgroupId: model.groupID,
-          userId: student.userId,
-          user: student,
-          studentgroup: model
+          students.forEach(function (student) {
+            if (model.memberships.findBy('user._belongsToState.canonicalState.id', student.get('id'))) {
+              freeStudents.removeObject(student);
+            }
+          });
+          return freeStudents;
         });
-        membership.save().catch((reason) => {
-          membership.deleteRecord();
-        });
-      },
-      goBack() {
-        var model = this.get('model');
-        this.transitionToRoute('studentgroups.show', model.id);
-      },
+      }
+    },
+    addStudent(student) {
+      var model = this.get('model');
+
+      var membership = this.store.createRecord('membership', {
+        studentgroupId: model.groupID,
+        userId: student.userId,
+        user: student,
+        studentgroup: model
+      });
+      membership.save().catch((reason) => {
+        membership.deleteRecord();
+      });
+    },
+    removeStudent(membership) {
+      membership.deleteRecord();
+      membership.get('isDeleted');
+      membership.save();
+    },
+    goBack() {
+      var model = this.get('model');
+      this.transitionToRoute('studentgroups.show', model.id);
+    },
     willTransition(transition) {
       if (this.currentModel.isNew && !this.currentModel.isSaving) {
         transition.abort();
