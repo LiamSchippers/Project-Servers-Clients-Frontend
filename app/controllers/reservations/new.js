83226024ba7a3fb24545a70205eacc219b15ce7c
Boris Bolder <borisb_2@hotmail.com>
2019-01-19 17:49:32 +0100

    End hour validation

--- app/controllers/reservations/new.js
+++ app/controllers/reservations/new.js
@@ -4,24 +4,32 @@ import {computed} from '@ember/object';
 
 export default Controller.extend({
   store: service(),
-  studentgroups: computed(function() {
+  studentgroups: computed(function () {
     return this.get('store').findAll('studentgroup');
   }),
 
-  enableGrid : function(){
-    if (this.get('model').get('endHour') !==undefined && this.get('model').get('startHour')!==undefined && this.get('model').get('day')!==undefined){
+  enableGrid: function () {
+    if (this.get('model').get('endHour') !== undefined && this.get('model').get('startHour') !== undefined && this.get('model').get('day') !== undefined) {
       this.set("toggleGridBlock", true);
-    }
-    else{
+    } else {
       this.set("toggleGridBlock", false);
     }
   },
-
-  classrooms: computed(function() {
-    return this.get('store').findAll('classroom');
+  minEndHour: computed('startHour', function () {
+    let startHour = +this.get('model').get('startHour') +1;
+    let options = [];
+    let times = ["08:30", "09:15", "10:15", "11:00", "11:45", "12:30", "13:15", "14:00", "15:00",
+      "15:45", "16:30", "17:15", "18:15", "19:00", "20:00", "20:45", "21:30"];
+    let arrayIndex = 0;
+    for (let i = startHour; i < 18; i++) {
+      options[arrayIndex] = {value: i, time: times[i-1]};
+      arrayIndex++;
+    }
+    return options;
   }),
-  minHour: computed('startHour', function () {
-    return this.get('startHour');
+
+  classrooms: computed(function () {
+    return this.get('store').findAll('classroom');
   }),
   actions: {
     selectStudentGroup: function (studentgroup) {
