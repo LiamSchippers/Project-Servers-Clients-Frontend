94ed11d0ebf3861b4369685a7ed4146b575e23d4
Midas <info@midasminnegal.nl>
2019-01-19 20:04:28 +0100

    Cleaned up code and added front end validation for max hours per reservation

--- app/controllers/reservations/new.js
+++ app/controllers/reservations/new.js
@@ -9,25 +9,22 @@ export default Controller.extend({
   }),
 
   enableGrid: function () {
-    if (this.get('model').get('endHour') !== undefined && this.get('model').get('startHour') !== undefined && this.get('model').get('day') !== undefined) {
-      this.set("toggleGridBlock", true);
-    } else {
-      this.set("toggleGridBlock", false);
-    }
+    const gridblockStatus = this.get('model').get('endHour') !== undefined && this.get('model').get('startHour') !== undefined && this.get('model').get('day') !== undefined;
+    this.set("toggleGridBlock", gridblockStatus);
   },
   minEndHour: computed('startHour', function () {
-    let startHour = +this.get('model').get('startHour') +1;
+    const startHour = +this.get('model').get('startHour') + 1;
     let options = [];
-    let times = ["08:30", "09:15", "10:15", "11:00", "11:45", "12:30", "13:15", "14:00", "15:00",
+    const times = ["08:30", "09:15", "10:15", "11:00", "11:45", "12:30", "13:15", "14:00", "15:00",
       "15:45", "16:30", "17:15", "18:15", "19:00", "20:00", "20:45", "21:30"];
-    let arrayIndex = 0;
-    for (let i = startHour; i < 18; i++) {
-      options[arrayIndex] = {value: i, time: times[i-1]};
-      arrayIndex++;
+    for (let i = startHour - 1; i < times.length; i++) {
+      options.push({value: i, time: times[i]});
     }
     return options;
   }),
-
+  errorMessage: computed('errorMessageText', function() {
+      return this.get('errorMessageText');
+  }),
   classrooms: computed(function () {
     return this.get('store').findAll('classroom');
   }),
@@ -55,13 +52,18 @@ export default Controller.extend({
       this.enableGrid();
     },
     saveModel() {
-      console.log(this.get('model'));
-      // label definieren
-      let label = this.get('model').get('day') + this.get('model').get('studentgroup').get('groupName');
-      this.get('model').set('label', label);
-      this.get('model').save().then((e) => {
-        this.transitionToRoute('index');
-      });
+      const studentGroup = this.get('model').get('studentgroup');
+      const maxReservableHours = studentGroup.get('reservableHours');
+      const reservedHours = this.get('model').get('endHour') - this.get('model').get('startHour');
+      if(maxReservableHours >= reservedHours) {
+        const reservationLabel = studentGroup.get('groupName') + this.get('model').get('day');
+        this.get('model').set('label', reservationLabel);
+        this.get('model').save().then((e) => {
+          this.transitionToRoute('index');
+        });
+      } else {
+        this.set('errorMessageText', "Het maximale aantal les uren dat je met deze groep per keer mag boeken is " + maxReservableHours);
+      }
     }
   }
 });
