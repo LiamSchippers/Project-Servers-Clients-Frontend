90d0b999610706484358b87fef20b77d6355bc9c
Gijs97 <gijs.abbring@live.nl>
2019-01-22 12:56:28 +0100

    groups block

--- app/components/my-groups-block.js
+++ app/components/my-groups-block.js
@@ -1,6 +1,12 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  session:  service(),
+  memberships: computed('session', function() {
+    return this.get('session.data.currentUser').memberships;
+  }),
   actions: {
     expandGroup() {
       this.toggleProperty('showGroupMembers');
