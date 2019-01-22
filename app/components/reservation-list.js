f934cb1cc5a7c86f6d3b9400ebe8ee95c1bb3fb4
jaspersauerwald <jaspersauerwald@gmail.com>
2019-01-22 11:11:33 +0100

    Personal reservations overview
    -styling gefixt
    -haalt reserveringen op van de database
    -en sorteert hem

--- app/components/reservation-list.js
+++ app/components/reservation-list.js
@@ -3,6 +3,20 @@ import EmberObject, { computed } from '@ember/object';
 
 export default Component.extend({
   limitedReservations: computed('reservations', function (){
-    return this.get('reservations').sortBy('day').slice(0, 3);
-  })
+    if (this.get('size')) {
+      return this.get('reservations').sortBy('day').slice(0, this.get('size'));
+    } else {
+      return this.get('reservations').sortBy('day');
+    }
+  }),
+
+  actions: {
+    // todo: alleen voor admin
+    deleteReservation(reservation){
+      /*reservation.deleteRecord();
+      reservation.get('isDeleted');
+      reservation.save();*/
+      this.delete();
+    }
+  }
 });
--- app/routes/reservations/index.js
+++ app/routes/reservations/index.js
@@ -4,13 +4,4 @@ export default Route.extend({
   model() {
     return this.store.findAll('reservation');
   },
-
-  actions: {
-    // todo: alleen voor admin
-    deleteReservation(reservation){
-      reservation.deleteRecord();
-      reservation.get('isDeleted');
-      reservation.save();
-    }
-  }
 });
