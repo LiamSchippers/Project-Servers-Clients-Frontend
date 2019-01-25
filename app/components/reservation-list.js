d36edaa9134bb76d1996b8d0f4cafd2026da7d2f
jaspersauerwald <jaspersauerwald@gmail.com>
2019-01-25 16:57:22 +0100

    getting user for filtering reservations

--- app/components/reservation-list.js
+++ app/components/reservation-list.js
@@ -4,19 +4,20 @@ import { inject } from '@ember/service';
 
 export default Component.extend({
 
-  // session: inject('session'),
+  session: inject('session'),
   // currentUser: computed(' session', function() {
   //   return this.get('session.data.currentUser');
   // }),
 
-  limitedReservations: computed('reservations', 'reservations.[]', function (){
-    currentUser: computed('session', function () {
-      return this.limitedReservations.get('session.data.currentUser');
-    });
+  limitedReservations: computed('reservations', 'reservations.[]', 'session', function (){
+    const currentUser = this.get('session.data.currentUser');
+    const memberships = currentUser.get('memberships');
+    const groups = memberships.get('studentgroupid');
     if (this.get('size')) {
       return this.get('reservations').sortBy('day').slice(0, this.get('size'));
     } else {
-      return this.get('reservations').sortBy('day');
+      return this.get('reservations').filterBy(currentUser.get('groups'));
+      //return this.get('reservations').sortBy('day');
     }
   }),
   actions: {
---
+++ tests/integration/helpers/period-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Helper | period', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{period inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), '1234');
+  });
+});
