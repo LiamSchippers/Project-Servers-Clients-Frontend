fc064c14f566295cfa99783d69605a0646bbfc42
jaspersauerwald <jaspersauerwald@gmail.com>
2019-01-27 20:02:13 +0100

    Personal reservations overview
    -removed filtering for personal reservations

--- app/components/reservation-list.js
+++ app/components/reservation-list.js
@@ -8,17 +8,6 @@ export default Component.extend({
   store: inject(),
 
   limitedReservations: computed('reservations', 'reservations.[]', 'session', function () {
-    // this.get('store').find('extended-user', this.get('session.data.authenticated.userId')).then(function(user) {
-    //   console.log(user);
-    // });
-    // let store = this.get('store');
-    // let reservations = this.get('store').find('membership', this.get('session.data.authenticated.userId')).then(function (membership) {
-    //   console.log(membership.get('studentgroupid'));
-    //   //return store.findAll('reservation');
-    //   return store.find('reservation', membership.get('studentgroupid')).sortBy('day');
-    // });
-    // return reservations;
-
     if (this.get('size')) {
       return this.get('reservations').sortBy('day').slice(0, this.get('size'));
     } else {
