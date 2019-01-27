4721a1c249942d7ca6ce8b466aab98f273815a86
jaspersauerwald <jaspersauerwald@gmail.com>
2019-01-27 13:58:25 +0100

    Personal reservations overview
    -attempt at filtering it for personal reservations
    -fixed user profile showing reservations, user profile now does not show reservations anymore

--- app/components/reservation-list.js
+++ app/components/reservation-list.js
@@ -1,28 +1,33 @@
 import Component from '@ember/component';
-import EmberObject, { computed } from '@ember/object';
-import { inject } from '@ember/service';
+import EmberObject, {computed} from '@ember/object';
+import {inject} from '@ember/service';
 
 export default Component.extend({
 
   session: inject('session'),
-  // currentUser: computed(' session', function() {
-  //   return this.get('session.data.currentUser');
-  // }),
+  store: inject(),
 
-  limitedReservations: computed('reservations', 'reservations.[]', 'session', function (){
-    const currentUser = this.get('session.data.currentUser');
-    const memberships = currentUser.get('memberships');
-    const groups = memberships.get('studentgroupid');
+  limitedReservations: computed('reservations', 'reservations.[]', 'session', function () {
+    // this.get('store').find('extended-user', this.get('session.data.authenticated.userId')).then(function(user) {
+    //   console.log(user);
+    // });
+    // let store = this.get('store');
+    // let reservations = this.get('store').find('membership', this.get('session.data.authenticated.userId')).then(function (membership) {
+    //   console.log(membership.get('studentgroupid'));
+    //   //return store.findAll('reservation');
+    //   return store.find('reservation', membership.get('studentgroupid')).sortBy('day');
+    // });
+    // return reservations;
+
     if (this.get('size')) {
       return this.get('reservations').sortBy('day').slice(0, this.get('size'));
     } else {
-      return this.get('reservations').filterBy(currentUser.get('groups'));
-      //return this.get('reservations').sortBy('day');
+      return this.get('reservations').sortBy('day');
     }
   }),
   actions: {
     // todo: alleen voor admin
-    deleteReservation(reservation){
+    deleteReservation(reservation) {
       reservation.deleteRecord();
       reservation.get('isDeleted');
       reservation.save();
