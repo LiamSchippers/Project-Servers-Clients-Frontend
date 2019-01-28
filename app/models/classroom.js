57c8476ef32ffeab92633719be9864d4618852de
Ruben Smit <RubenSmit@users.noreply.github.com>
2019-01-29 00:47:51 +0100

    Improved defaults

--- app/models/classroom.js
+++ app/models/classroom.js
@@ -4,9 +4,9 @@ export default DS.Model.extend({
   roomNumber: DS.attr('string'),
   building: DS.attr('string', { defaultValue: 'Epy Drost' }),
   mainClassroomType: DS.attr('string', { defaultValue: 'Onderwijsruimte' }),
-  kind: DS.attr('string', { defaultValue: 'Onderwijsruimte' }),
+  kind: DS.attr('string', { defaultValue: 'Specifiek' }),
   type: DS.attr('string', { defaultValue: 'apple' }),
-  capacity: DS.attr('number'),
+  capacity: DS.attr('number', { defaultValue: 25 }),
   reservations: DS.hasMany('reservation'),
   availableSpots: DS.attr('number'),
 });
