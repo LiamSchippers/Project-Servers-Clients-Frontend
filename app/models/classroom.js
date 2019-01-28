75dc11d80fc1ce1f2d7a5cd1e2037cbeb289a3ba
Ruben Smit <RubenSmit@users.noreply.github.com>
2019-01-29 00:28:39 +0100

    Added defaults to classroom

--- app/models/classroom.js
+++ app/models/classroom.js
@@ -2,10 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   roomNumber: DS.attr('string'),
-  building: DS.attr('string'),
-  mainClassroomType: DS.attr('string'),
-  kind: DS.attr('string'),
-  type: DS.attr('string'),
+  building: DS.attr('string', { defaultValue: 'Epy Drost' }),
+  mainClassroomType: DS.attr('string', { defaultValue: 'Onderwijsruimte' }),
+  kind: DS.attr('string', { defaultValue: 'Onderwijsruimte' }),
+  type: DS.attr('string', { defaultValue: 'apple' }),
   capacity: DS.attr('number'),
   reservations: DS.hasMany('reservation'),
   availableSpots: DS.attr('number'),
