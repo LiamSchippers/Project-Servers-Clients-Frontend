43dcf529611e2e209e341630af2e24f6f11085ab
Ruben Smit <RubenSmit@users.noreply.github.com>
2019-01-29 20:29:29 +0100

    Added member of groups list

--- app/components/my-groups-block.js
+++ app/components/my-groups-block.js
@@ -1,6 +1,13 @@
 import Component from '@ember/component';
+import {computed} from '@ember/object';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+  session: inject(),
+  store: inject(),
+  currentUser: computed('session.data.authenticated.userId', function () {
+    return this.get('store').find('extended-user', this.get('session.data.authenticated.userId'));
+  }),
   actions: {
     expandGroup() {
       this.toggleProperty('showGroupMembers');
