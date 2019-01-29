a232db92e53fa2aada2ede05ca0ed9891d1a6ea0
Ruben Smit <RubenSmit@users.noreply.github.com>
2019-01-29 16:13:26 +0100

    Added more fields to classroom table

--- app/models/classroom.js
+++ app/models/classroom.js
@@ -3,9 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   roomNumber: DS.attr('string'),
   building: DS.attr('string', { defaultValue: 'Epy Drost' }),
-  mainClassroomType: DS.attr('string', { defaultValue: 'Onderwijsruimte' }),
-  kind: DS.attr('string', { defaultValue: 'Specifiek' }),
-  type: DS.attr('string', { defaultValue: 'apple' }),
+  mainClassroomType: DS.attr('string', { defaultValue: 'Onderwijsruimte' }), // hoofdtype
+  kind: DS.attr('string', { defaultValue: 'Specifiek' }), // subtype
+  type: DS.attr('string', { defaultValue: 'apple' }), // faciliteiten
   capacity: DS.attr('number', { defaultValue: 25 }),
   reservations: DS.hasMany('reservation'),
   availableSpots: DS.attr('number'),
